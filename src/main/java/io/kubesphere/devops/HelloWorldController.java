dc5eb49ab903d287fbe1e0ba5612acb286b9f775
bigforsee <79566322+bigforsee@users.noreply.github.com>
2021-02-25 08:29:51 +0800

    Update HelloWorldController.java

--- src/main/java/io/kubesphere/devops/HelloWorldController.java
+++ src/main/java/io/kubesphere/devops/HelloWorldController.java
@@ -14,6 +14,7 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String sayHello() {
+        String a="Really appreciate your star, that's the power of our life.";
         return "Really appreciate your star, that's the power of our life.";
     }
 }
